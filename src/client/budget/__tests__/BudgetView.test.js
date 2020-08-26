d67c2d54bd27d131ab1a9cb9b5ef785a99ab5124
Jeff <JeffreyRiggle@gmail.com>
2020-08-26 06:32:38 -0400

    add category view tests

--- src/client/budget/__tests__/BudgetView.test.js
+++ src/client/budget/__tests__/BudgetView.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import BudgetView from '../BudgetView';
 import { filteredBudgetItems, getExpectedIncome } from '../../../common/eventNames';
 import { BrowserRouter } from 'react-router-dom';
---
+++ src/client/budget/__tests__/CategoryView.test.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import CategoryView from '../CategoryView';
+import { filteredBudgetItems, getCategory } from '../../../common/eventNames';
+import { BrowserRouter } from 'react-router-dom';
+import { client } from '@jeffriggle/ipc-bridge-client';
+
+jest.mock('@jeffriggle/ipc-bridge-client', () => ({
+    client: {
+        sendMessage: jest.fn((eventName) => {
+            if (eventName === 'filteredBudgetItems') {
+                return Promise.resolve([
+                    {
+                        id: 1,
+                        category: 'Food',
+                        amount: 10000
+                    },
+                    {
+                        id: 2,
+                        category: 'Food',
+                        amount: 5000
+                    }
+                ]);
+            }
+
+            if (eventName === 'getCategory') {
+                return Promise.resolve({
+                    name: 'Food',
+                    allocated: 20000
+                });
+            }
+        })
+    }
+}));
+
+describe('Category View', () => {
+    let component;
+
+    beforeEach(() => {
+        const mockMatch = {
+            params: {
+                id: 'Food'
+            }
+        };
+
+        component = render(<BrowserRouter><CategoryView match={mockMatch}/></BrowserRouter>);
+    });
+
+    it('should get budget items', () => {
+        expect(client.sendMessage).toHaveBeenCalledWith(filteredBudgetItems, expect.anything());
+    });
+
+    it('should get the category', () => {
+        expect(client.sendMessage).toHaveBeenCalledWith(getCategory, expect.objectContaining({
+            category: 'Food',
+            includeRollover: true
+        }));
+    });
+
+    it('should have the correct budget items', () => {
+        expect(component.container.querySelectorAll('tbody tr').length).toBe(2);
+    });
+
+    it('should have the correct target', () => {
+        expect(component.getByTestId('category-target').textContent).toBe('Target $200.00');
+    });
+
+    it('should have the correct spend', () => {
+        expect(component.getByTestId('category-spend').textContent).toBe('Total Spent $150.00');
+    });
+});
