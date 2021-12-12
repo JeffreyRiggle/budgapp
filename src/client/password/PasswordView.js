62eefcd7f1b462436bd9a36239c2ce0200c7e7ac
Jeff <JeffreyRiggle@gmail.com>
2021-12-12 11:17:57 -0500

    migrate password view

--- src/client/password/PasswordView.js
+++ src/client/password/PasswordView.js
@@ -1,70 +1,57 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {withRouter} from 'react-router-dom';
 import passwordService from '../services/passwordService';
 import './Password.scss';
 
-class PasswordView extends Component {
-    constructor(props) {
-        super(props);
+const PasswordView = (props) => {
+    const [password, setPassword] = React.useState('');
+    const [failed, setFailed] = React.useState(false);
 
-        this.state = {
-            password: '',
-            failed: false
-        };
-    }
+    const passwordChanged = React.useCallback((event) => {
+        setPassword(event.target.value);
+    });
 
-    render() {
-        return (
-            <div className="password-view">
-                <h1>Password Protected</h1>
-                <div className="info-area">
-                    <p>This file is password protected, please enter password to manage file.</p>
-                    {this.state.failed && <p className="error">Provided password was incorrect please try again.</p>}
-                </div>
-                <div className="submit-area">
-                    <input 
-                        className="password-field" 
-                        type="password" 
-                        value={this.state.password} 
-                        onChange={this.passwordChanged.bind(this)}
-                        onKeyPress={this.handleKeyPress.bind(this)}>
-                    </input>
-                    <button className="submit-btn" onClick={this.sendPassword.bind(this)}>Submit</button>
-                </div>
-            </div>
-        )
-    }
-
-    handleKeyPress(event) {
-        if (event.key === 'Enter') {
-            this.sendPassword();
-        }
-    }
-
-    passwordChanged(event) {
-        this.setState({
-            password: event.target.value
-        });
-    }
-
-    sendPassword() {
-        this.setState({
-            password: ''
-        });
-
-        passwordService.sendPassword(this.state.password, this.passwordProvided.bind(this));
-    }
-
-    passwordProvided(result) {
+    function passwordProvided(result) {
         if (result.success) {
-            this.props.history.push('/');
+            props.history.push('/');
             return;
         }
 
-        this.setState({
-            failed: true
-        });
+        setFailed(true);
     }
+
+    function sendPassword() {
+        setPassword('');
+        passwordService.sendPassword(password, passwordProvided);
+    }
+
+    const handleKeyPress = React.useCallback((event) => {
+        if (event.key !== 'Enter') {
+            return;
+        }
+
+        sendPassword();
+    });
+
+    return (
+        <div className="password-view">
+            <h1>Password Protected</h1>
+            <div className="info-area">
+                <p>This file is password protected, please enter password to manage file.</p>
+                {failed && <p className="error">Provided password was incorrect please try again.</p>}
+            </div>
+            <div className="submit-area">
+                <input 
+                    className="password-field" 
+                    type="password" 
+                    value={password} 
+                    onChange={passwordChanged}
+                    onKeyPress={handleKeyPress}>
+                </input>
+                <button className="submit-btn" onClick={sendPassword}>Submit</button>
+            </div>
+        </div>
+    )
 }
 
 export default withRouter(PasswordView);
