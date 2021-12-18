d97ed6861358e977a1b0abcf770dc575fce705ea
Jeff <JeffreyRiggle@gmail.com>
2021-12-18 13:48:00 -0500

    fix linting issue

--- src/client/general/GeneralView.jsx
+++ src/client/general/GeneralView.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { client } from '@jeffriggle/ipc-bridge-client';
 import CategoryConfiguration from './CategoryConfiguration';
