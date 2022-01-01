cefbb47ae7bfa1301aa59437b3125e23c645cee8
Jeff <JeffreyRiggle@gmail.com>
2022-01-01 09:53:28 -0500

    fix failing e2e test

--- test/models/history.js
+++ test/models/history.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const { Budget } = require("./budget");
 const { Income } = require('./income');
 
@@ -6,25 +7,46 @@ class History {
         this.client = client;
     }
 
+    async findTargetMonth(month, historyItems) {
+        for (let item of historyItems) {
+            const domItem = await item.$$('td');
+            const text = await domItem[0].getText();
+            if (text === month) {
+                return item;
+            }
+        }
+    }
+
+    async findLastMonth(historyItems) {
+        return this.findTargetMonth(moment(new Date()).subtract(1, 'M').format('MMMM YY'), historyItems);
+    }
+
+    async findCurrentMonth(historyItems) {
+        return this.findTargetMonth(moment(new Date()).format('MMMM YY'), historyItems);
+    }
+
     async getMargin() {
         const historyTable = await this.client.$('table tbody');
         const historyItems = await historyTable.$$('tr');
-        const currentMonth = await historyItems[0].$$('td');
+        const currentMonthRow = await this.findCurrentMonth(historyItems);
+        const currentMonth = await currentMonthRow.$$('td');
         return await currentMonth[3].getText();
     }
 
     async getLastMonthsMargin() {
         const historyTable = await this.client.$('table tbody');
         const historyItems = await historyTable.$$('tr');
-        const currentMonth = await historyItems[1].$$('td');
-        return await currentMonth[3].getText();
+        const lastMonth = await this.findLastMonth(historyItems);
+        const lastMonthItems = await lastMonth.$$('td');
+        return await lastMonthItems[3].getText();
     }
 
     async goToLastMonthsIncome() {
         const historyTable = await this.client.$('table tbody');
         const historyItems = await historyTable.$$('tr');
-        const currentMonth = await historyItems[1].$$('td');
-        const link = await currentMonth[1].$('a');
+        const lastMonth = await this.findLastMonth(historyItems);
+        const lastMonthItems = await lastMonth.$$('td');
+        const link = await lastMonthItems[1].$('a');
         await link.click();
         return new Income(this.client);
     }
@@ -32,8 +54,9 @@ class History {
     async goToLastMonthsBudget() {
         const historyTable = await this.client.$('table tbody');
         const historyItems = await historyTable.$$('tr');
-        const currentMonth = await historyItems[1].$$('td');
-        const link = await currentMonth[2].$('a');
+        const lastMonth = await this.findLastMonth(historyItems);
+        const lastMonthItems = await lastMonth.$$('td');
+        const link = await lastMonthItems[2].$('a');
         await link.click();
         return new Budget(this.client);
     }
