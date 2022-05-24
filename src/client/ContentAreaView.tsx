4a03b71541923308d52a6dd901125976c6a4436c
Jeff <JeffreyRiggle@gmail.com>
2022-05-24 05:27:55 -0400

    chore(budgapp): Upgrade electron version

--- src/client/ContentAreaView.tsx
+++ src/client/ContentAreaView.tsx
@@ -57,18 +57,18 @@ const ContentAreaView = (props: ContentAreaViewProps) => {
                 </ul>
             </div>) }
             <div className="content-area">
-                <Route exact path="/" component={GeneralView}/>
-                <Route exact path="/budget" component={BudgetView}/>
-                <Route path="/budget/:date" component={BudgetView}/>
-                <Route exact path="/income" component={IncomeView}/>
-                <Route path="/income/:date" component={IncomeView}/>
-                <Route path="/addIncome" component={AddIncomeView}/>
-                <Route path="/history" component={HistoryView}/>
-                <Route path="/addBudget" component={AddBudgetItems}/>
-                <Route exact path="/category/:id" component={CategoryView}/>
-                <Route path="/category/:id/:date" component={CategoryView}/>
-                <Route path="/password" component={PasswordView}/>
-                <Route path="/storage" component={StorageView}/>
+                <Route exact path="/" component={(props: any) => <GeneralView {...props} />}/>
+                <Route exact path="/budget" component={(props: any) => <BudgetView {...props} />}/>
+                <Route path="/budget/:date" component={(props: any) => <BudgetView {...props} />}/>
+                <Route exact path="/income" component={(props: any) => <IncomeView {...props} />}/>
+                <Route path="/income/:date" component={(props: any) => <IncomeView {...props} />}/>
+                <Route path="/addIncome" component={(props: any) => <AddIncomeView {...props} />}/>
+                <Route path="/history" component={(props: any) => <HistoryView {...props} />}/>
+                <Route path="/addBudget" component={(props: any) => <AddBudgetItems {...props} />}/>
+                <Route exact path="/category/:id" component={(props: any) => <CategoryView {...props} />}/>
+                <Route path="/category/:id/:date" component={(props: any) => <CategoryView {...props} />}/>
+                <Route path="/password" component={(props: any) => <PasswordView {...props} />}/>
+                <Route path="/storage" component={(props: any) => <StorageView {...props} />}/>
             </div>
         </div>
     );
