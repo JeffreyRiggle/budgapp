266780a11af24ea97f27de05570c56c16dafdb9a
Jeff <JeffreyRiggle@gmail.com>
2022-06-15 05:25:36 -0400

    fix(budgapp): Attempt new macos path

--- test/wdio.conf.ts
+++ test/wdio.conf.ts
@@ -2,7 +2,7 @@ function getElectronBinary() {
     const basePath = './node_modules/electron/dist/electron';
 
     if (process.platform === 'darwin') {
-        return './node_modules/electron/dist/Electron.app';
+        return './node_modules/electron/dist/Electron.app/Contents/MacOS/Electron';
     }
     
     if (process.platform === 'win32') {
