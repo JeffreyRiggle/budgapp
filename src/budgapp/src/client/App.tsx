11616e17856a33227018dd093cc5d40e0212b59e
Jeff <JeffreyRiggle@gmail.com>
2022-11-25 20:30:36 -0500

    feat(budgapp): Get basic navigation working better on mobile

--- src/budgapp/src/client/App.tsx
+++ src/budgapp/src/client/App.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, NavLink } from 'react-router-dom';
 import ContentAreaView from './ContentAreaView';
 
 import './App.scss';
+import useMobileBreakpoint from './hooks/use-mobile-breakpoint';
 
 const App = () => {
+  const isMobile = useMobileBreakpoint();
+  const [showPopup, setPopup] = React.useState(false);
+
   return (
     <BrowserRouter>
       <div className="App">
         <header className="header-area">
+          { isMobile && (
+            <div className="mobile-nav">
+              <button onClick={() => setPopup(true)}>Nav</button>
+              { showPopup && (
+                <div className="mobile-nav-area">
+                  <ul className="mobile-nav-items">
+                    <li><NavLink exact to="/" className="sidebar-item">General</NavLink></li>
+                    <li><NavLink to="/budget" className="sidebar-item">Budget</NavLink></li>
+                    <li><NavLink to="/income" className="sidebar-item">Income</NavLink></li>
+                    <li><NavLink to="/history" className="sidebar-item">History</NavLink></li>
+                  </ul>
+                  <div className="close-area">
+                    <button onClick={() => setPopup(false)}>Close</button>
+                  </div>
+                </div>
+              )}
+            </div>
+          )}
           Budgapp
         </header>
         <ContentAreaView />
